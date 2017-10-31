937576c7ed518ae6a6bbd8c011d809d5d9a7f1f3
Stephanie McNaught <StephanieM@DHICorp.com>
2017-10-31 10:40:49 -0600

    stopped hard coding the port number

--- server.js
+++ server.js
@@ -172,6 +172,6 @@ app.put('/addNote', function (req, res){
 
 
 // Set the app to listen on port 3000
-app.listen(3000, function () {
-    console.log("App running on port 3000!");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("App running");
 });
